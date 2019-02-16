80551d0c437e1a26abdd942fb217fede0048c9f0
GreedyCoding <marco_hofer2012@outlook.com>
2019-02-17 00:53:17 +0100

    Implemented BinarySearch and test class for Search in CodingChallanges.Tests

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -9,13 +9,16 @@ namespace CodingChallenges
     {
         static void Main(string[] args)
         {
-            //* Sorting Algorithm Tests
+            //*/ Sorting Algorithm Tests
             Tests.Sort.Bubble();
             Log.Line(ConsoleColor.Magenta);
             Tests.Sort.Selection();
             Log.Line(ConsoleColor.Magenta);
             Tests.Sort.Insertion();
-            //*/
+            //*/ Search Algorithm Tests
+            Log.Line(ConsoleColor.Magenta);2
+            Tests.Search.Binary();
+
 
             Console.ReadLine();
         }
---
+++ CodingChallenges/Tests/Search.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodingChallenges.Utilities;
+using CodingChallenges.SortingAlgorithms;
+using CodingChallenges.SearchAlgorithms;
+
+namespace CodingChallenges.Tests
+{ 
+    internal static class Search
+    {
+        public static void Binary()
+        {
+            Log.Title("Binary Search");
+            int tries = 5;
+            Console.WriteLine($"You have '{tries}' tries to check an array with 50 randoms ints for an integer.");
+            for (int i = 0; i < 5; i++)
+            {
+                int[] sortedArray = BubbleSort.SortArray(Sorting.RandomIntArray(50, 100), true, false);
+
+                Console.WriteLine("Please input an integer between 1 and 100");
+                int toSearchFor = Convert.ToInt32(Console.ReadLine());
+
+                int indexOfValue = BinarySearch.Search(sortedArray, toSearchFor);
+
+                if (indexOfValue == -1)
+                {
+                    Console.WriteLine("The provided value was not found in the array");
+                }
+                else
+                {
+                    Console.WriteLine($"The provided value '{toSearchFor}' was found at the following index: '{indexOfValue}'");
+                }
+            }
+
+
+        }
+    }
+}
