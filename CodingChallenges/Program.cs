7ffa27deede4cca3108b258828f8d35344efe22e
GreedyCoding <marco_hofer2012@outlook.com>
2019-02-16 15:11:42 +0100

    Cleaned up Main

---
+++ CodingChallenges/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using CodingChallenges.SortingAlgorithm;
+
+namespace CodingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tests.SortBubble();
+            Console.ReadLine();
+        }
+
+    }
+}
