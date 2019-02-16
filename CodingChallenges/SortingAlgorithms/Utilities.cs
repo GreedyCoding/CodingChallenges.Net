7bde85b835c67964c2a4c99fbebfc7c73ada2e48
GreedyCoding <marco_hofer2012@outlook.com>
2019-02-16 20:16:11 +0100

    Added Utilities Class to keep the project clean and updated tests to include selection sort

---
+++ CodingChallenges/SortingAlgorithms/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenges.SortingAlgorithms
+{
+    static class Utilities
+    {
+        public static int[] SwapElements(int[] array, int indexA, int indexB)
+        {
+            int temp = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = temp;
+            return array;
+        }
+
+        public static List<int> SwapElements(List<int> list, int indexA, int indexB)
+        {
+            int temp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = temp;
+            return list;
+        }
+
+        public static void LogContents(int[] array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+        }
+
+        public static void LogContents(List<int> array)
+        {
+            foreach (int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
