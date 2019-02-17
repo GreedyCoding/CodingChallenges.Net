2f803379884545d5f0d2c2fdc3b1b7e2fd404adb
GreedyCoding <marco_hofer2012@outlook.com>
2019-02-17 20:41:16 +0100

    Started implementation of the BinaryTree

---
+++ CodingChallenges/BinaryContainer/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenges.BinaryContainer.BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+    }
+}
---
+++ CodingChallenges/BinaryContainer/BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenges.BinaryContainer.BinarySearchTree
+{
+    class Node
+    {
+        int _data;
+        Node _left;
+        Node _right;
+
+        public Node(int item)
+        {
+            _data = item;
+            _left = null;
+            _right = null;
+        }
+    }
+}
